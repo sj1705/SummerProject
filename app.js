0d50a2768e59b63db8eb62fc375bb950157874e8
Shrestha Jaiswal <shresthajaiswal05.com>
2022-07-01 22:07:50 +0530

    new signup page

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+//jshint esversion:6
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({}));
+const _ = require("lodash");
 
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
 
@@ -11,7 +16,18 @@ app.use(express.static(__dirname));
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+app.post("/signup", function(req, res) {
+  var customerInfo = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email
+  }
+  console.log(customerInfo);
+  res.redirect("/");
+});
 
 
 
