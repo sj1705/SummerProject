41b21941715eb86a432f2569f0e9ff6668af8c97
Shrestha Jaiswal <shresthajaiswal05.com>
2022-07-18 03:23:08 +0530

    vendor signup works

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const _ = require("lodash");
+const multer = require("multer");
+const path = require("path");
+// const upload = multer({'./uploads'});
 mongoose.connect("mongodb+srv://admin:setuservice@cluster0.4pjko.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true
 });
 
@@ -23,11 +26,36 @@ const customerSchema = {
   data: String
 };
 const vendorSchema = {
-
+    ServiceName: String,
+    BusinessName: String,
+    OwnerName: String,
+    OwnerEmail: String,
+    Refno: Number,
+    Address: String,
+    State: String,
+    City: String,
+    OpeningTime: String,
+    ClosingTime: String,
+    Discription: String,
+    Amount: Number,
+    Payment: String,
+    Image: []
 };
 const Customer = mongoose.model('Customer', customerSchema);
+const Vendor = mongoose.model('Vendor', vendorSchema);
 var displayName = "Sign Up/Sign In";
 var message = "";
+var imagepath=[];
+var i=0;
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() +path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage }).array('Images',5);
+
 
 app.get("/signup", function(req, res) {
   res.render("signup");
@@ -69,13 +97,55 @@ app.get("/", function(req, res) {
   res.render("index", {
     displayName: displayName
   });
-  // console.log(req.body.inputBox.value);
-
 });
 app.post("/message", function(req, res) {
   res.redirect("/");
 });
 
+app.get("/vendor-signup", function(req, res) {
+  res.render("vendor-signup");
+});
+
+app.get("/vendor-signup", function(req, res) {
+  res.render("vendor-signup");
+});
+
+app.post("/vendor-signup", upload ,function(req, res) {
+for(i=0;i<req.files.length;i++){
+  imagepath[i]=req.files[i].filename;
+}
+
+  const vendorInfo = new Vendor({
+    ServiceName: req.body.ServiceName,
+    BusinessName: req.body.BusinessName,
+    OwnerName: req.body.OwnerName,
+    OwnerEmail: req.body.OwnerEmail,
+    Refno: req.body.Refno,
+    Address: req.body.Address,
+    State: req.body.state,
+    City: req.body.city,
+    OpeningTime: req.body.OpeningTime,
+    ClosingTime: req.body.ClosingTime,
+    Discription: req.body.Discription,
+    Amount: req.body.Amount,
+    Payment: req.body.Payment,
+    Image: imagepath
+  });
+  Vendor.findOne({
+    OwnerEmail: req.body.OwnerEmail
+  }, function(err, foundVendor) {
+    if (!err) {
+      if (foundVendor== null) {
+         vendorInfo.save();
+         message = "You Signed Up...";
+         console.log(vendorInfo);
+      } else {
+        message = " email already exists in our record... ";
+      }
+    }
+    res.render("message",{message:message});
+  });
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running at 3000");
